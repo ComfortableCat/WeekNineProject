14b3eb88e3ab63277f89b26dcc94d1b6decad8ee
Bertie Raffle <bertierwork@gmail.com>
2024-12-01 22:08:37 +0000

    hide hidden inputs in comment form

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -22,28 +22,28 @@ export default function CommentForm({ topicId, parentId }) {
             <textarea maxLength={144} type="text" required />
           </Form.Control>
         </Form.Field>
-
-        <Form.Field>
-          <Form.Control asChild>
-            <input
-              type="text"
-              name="topicId"
-              value={topicId}
-              defaultValue={topicId}
-            />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field>
-          <Form.Control asChild>
-            <input
-              type="text"
-              name="parentId"
-              value={parentId}
-              defaultValue={parentId}
-            />
-          </Form.Control>
-        </Form.Field>
-
+        <VisuallyHidden>
+          <Form.Field>
+            <Form.Control asChild>
+              <input
+                type="text"
+                name="topicId"
+                value={topicId}
+                defaultValue={topicId}
+              />
+            </Form.Control>
+          </Form.Field>
+          <Form.Field>
+            <Form.Control asChild>
+              <input
+                type="text"
+                name="parentId"
+                value={parentId}
+                defaultValue={parentId}
+              />
+            </Form.Control>
+          </Form.Field>
+        </VisuallyHidden>
         <Form.Submit asChild>
           <button className="mt-2.5 box-border inline-flex h-[35px] w-full items-center justify-center rounded bg-white px-[15px] font-medium leading-none text-violet11 shadow-[0_2px_10px] shadow-blackA4 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
             send reply
